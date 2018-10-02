3a2f21c6b41f95fa56a00d6c70d591ac6ed34efd
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-10-01 22:09:20 -0600

    InvoiceItem show complete

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -11,4 +11,15 @@ describe "InvoiceItem API" do
     expect(response).to be_successful
     expect(invoice_items.count).to eq(5)
   end
+
+  it 'can get one invoice item by id' do
+    id = create(:invoice_item).id
+
+    get '/api/v1/invoice_items/1.json'
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice_item["id"]).to eq(id)
+  end
 end
