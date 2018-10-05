9ee7223475803c71bf446f719a24cbda5ce07060
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-10-04 20:43:31 -0600

    Completes Customer relationship endpoints

---
+++ spec/requests/api/v1/merchant/merchant_relationship_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Merchant Relationships API" do
+  it 'finds all associated items for a merchant' do
+    merchant = create(:merchant)
+    id = merchant.id
+
+    items = [create(:item), create(:item), create(:item)]
+
+    create_list(:item, 3)
+
+    merchant.items << items
+
+    get "/api/v1/merchants/#{id}/items"
+
+    merchant_items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant_items.count).to eq(3)
+  end
+end
