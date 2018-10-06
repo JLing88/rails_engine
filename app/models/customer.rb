a518ec627f110100a2016ba6c41279638e1932d7
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-10-06 11:32:07 -0600

    Customer.favorite_merchant working

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,17 @@ class Customer < ApplicationRecord
   validates_presence_of :last_name
 
   has_many :invoices
+  has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
+  has_many :merchants, through: :invoices
+
+  def self.favorite_merchant(id)
+    Merchant.select("merchants.*, count(merchants.id) AS count")
+            .joins(:transactions, :invoices)
+            .where("invoices.customer_id = ?", id)
+            .where("transactions.result = ?", "success")
+            .group("merchants.id")
+            .order(count: :desc)
+            .limit(1)
+  end
 end
